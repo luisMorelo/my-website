81d26b6444b540000cea9962a4489ad54655869f
Luis Morelo <luisalmopi@gmail.com>
2024-11-01 21:41:43 -0500

    vista de resumen agregada

--- mypagina/mysite/urls.py
+++ mypagina/mysite/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.my_vista, name= 'my vista principal')
+    path('', views.my_vista, name= 'my vista principal'),
+    path('resume', views.view_resume, name='resumen')
 ]
--- mypagina/mysite/views.py
+++ mypagina/mysite/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def my_vista(request):
     if request.method == 'GET':
         return render(request, 'index.html')
+    
+
+
+def view_resume(request):
+    if request.method == 'GET':
+        return render(request, 'resume.html')
