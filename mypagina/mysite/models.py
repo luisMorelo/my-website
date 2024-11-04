a28ac98da5fbafbede32e7a4173501e217a3d7d5
Luis Morelo <luisalmopi@gmail.com>
2024-11-03 23:02:11 -0500

    creacion del modelo y migraciones

--- mypagina/mysite/models.py
+++ mypagina/mysite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class correos_contacto(models.Model):
+    nombre = models.TextField()
+    correo = models.EmailField()
+    telephone = models.IntegerField()
+    description = models.TextField()
